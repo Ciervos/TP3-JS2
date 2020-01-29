8d3bd21d9fc9810d0f9af984d026a503b48eee75
Ciervos <55422681+Ciervos@users.noreply.github.com>
2020-01-29 15:43:39 -0300

    primer commit con la base

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
+
+/*Funciones A */
+
+/*Funciones B */
+
+/*Funciones C */
+
+/*Funciones D */
---
+++ index.test.js
@@ -0,0 +1,9 @@
+/* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
+
+/*Test A */
+
+/*Test B */
+
+/*Test C */
+
+/*Test D */
