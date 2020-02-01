e33f643694c0c7d28dedec0e6b47e3fc3a100020
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-02-01 18:03:33 -0300

    se agregó primera funcion de axios

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 /* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
-
-
+const baseUrl = " https://tp-js-2-api-wjfqxquokl.now.sh";
 /*Funciones A */
 
+let lista = [];
+let todo = {
+    name,
+    email,
+    address,
+    phone,
+};
 
+const handleError = err => {
+    alert (`Hubo un error. ${err}`)
+}
+
+const getTodos = async () => {
+    try {
+        const res = await axios.get(baseUrl);
+        lista = res.data;
+        showTodo();
+
+    } catch (err) {
+        handleError(err);
+    }
+ }
 /*Funciones B */
 
 /*Funciones C */
