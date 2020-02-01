cb393cfa420e76fcf38bef54051337f251f3256a
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-01-31 22:08:52 -0300

    se agregó funcionalidad de los 2 botones de eliminar en el modal de add

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 /* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
-const baseUrl = "https://tp-js-2-api-wjfqxquokl.now.sh";
-let lista = [];
-let newUser = {
-    fullname: "",
-    email: null,
-    address: null,
-    phone:""
-};
 
-const handleError = err => {
-    alert (`Hubo un error. ${err}`)
-}
+
 /*Funciones A */
 
 
