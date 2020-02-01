6a7db6d7174561279a56fe3fe80c54da0df94ee1
Ciervos <55422681+Ciervos@users.noreply.github.com>
2020-02-01 18:12:28 -0300

    dar funcion filtrar a boton

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
+
 /* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
+const baseUrl = "https://tp-js-2-api-wjfqxquokl.now.sh/users";
+let lista = [];
+const handleError = err =>{
+    alert(`Hubo un error. ${err}`);
+};
 
 /*Funciones A */
 
 /*Funciones B */
+/* accion filtrar */
+const getLista = async () => {
+    await axios.get(`${baseUrl}`)
+        .then(res => {
+            lista = res.data;
+        })
+        .catch(handleError);
+};
+
+const getBuscar = async q => {
+    await axios.get(`${baseUrl}/?search=${q}`)
+        .then(res => {
+            lista = res.data;
+        })
+        .catch(handleError);
+};
+
+
+
+const filtrar = () =>{
+    let q = document.querySelector("#input-filtro").value; 
+    getBuscar(q);
+}
+
+let botonBuscar = document.querySelector("#boton-filtro");
+botonBuscar.addEventListener("click", filtrar);
+
 
 /*Funciones C */
 
