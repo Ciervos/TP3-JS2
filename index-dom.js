1a44456fcc18050b390b7ea2bd029f7c2e901a77
Mary Angel Davila <mary.angel.davila@gmail.com>
2020-02-03 19:31:45 -0300

    Creando modal eliminar y editando estilos

--- index-dom.js
+++ index-dom.js
@@ -1,27 +1,132 @@
+/*Generar la tabla y lista en html */
+const showTodo = () => {
+  const tbody = document.querySelector(".tabla-contenido");
+  for (let todo of lista) {
+    const tr = document.createElement("tr");
+    tr.className = "tabla-contenido-fila";
 
+    const td = document.createElement("td");
+    td.className = "tabla-contenido-celda";
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    td.appendChild(input);
+
+    const td1 = document.createElement("td");
+    const spanName = document.createElement("span");
+    spanName.textContent = todo.fullname;
+    td1.appendChild(spanName);
+
+    const td2 = document.createElement("td");
+    const spanEmail = document.createElement("span");
+    spanEmail.textContent = todo.email;
+    td2.appendChild(spanEmail);
+
+    const td3 = document.createElement("td");
+    const spanAddress = document.createElement("span");
+    spanAddress.textContent = todo.address;
+    td3.appendChild(spanAddress);
+
+    const td4 = document.createElement("td");
+    const spanPhone = document.createElement("span");
+    spanPhone.textContent = todo.phone;
+    td4.appendChild(spanPhone);
+
+    const td5 = document.createElement("td");
+    const span = document.createElement("span");
+    span.className = "content-icons";
+    const i = document.createElement("i");
+    i.className = "fas fa-pencil-alt pencil";
+    span.appendChild(i);
+    const tacho = document.createElement("i");
+    tacho.className = "fas fa-trash-alt tacho";
+    span.appendChild(tacho);
+
+    tacho.addEventListener("click", () => {
+      tbody.textContent = "";
+      deleteTodo(deleteid, () => {
+        showTodo();
+      });
+    });
+
+    td5.appendChild(span);
+    tr.appendChild(td);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tbody.appendChild(tr);
+  }
+};
+showTodo();
+/*Fin de generar tabla */
+
+/*Boton eliminar empleado */
+const deleteTodo = () => {
+  let botonEliminarEmpleado = document.querySelectorAll(".tacho");
+  botonEliminarEmpleado.forEach(boton => {
+    boton.addEventListener("click", () => {
+      let modal = document.querySelector("#modal-eliminar");
+      modal.setAttribute("style", "display:block");
+    });
+  });
+};
+/*Boton fin eliminar empleado */
+
+/*Boton de header y agregar un nuevo empleado */
+const addBoton = document.querySelector("#todo-create");
+
+addBoton.addEventListener("click", async () => {
+  const fullname = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const address = document.querySelector("#address").value;
+  const phone = document.querySelector("#phone").value;
+  const tbody = document.querySelector(".tabla-contenido");
+  tbody.textContent = "";
+  await createTodo(fullname, email, address, phone);
+  showTodo();
+});
 
 const addNewEmployee = () => {
-   let botonAdd= document.querySelector('.wrapper-boton');
-   botonAdd.addEventListener('click', () => {
-    let modal = document.querySelector('.modal-wrapper');
-    modal.setAttribute('style', 'display:block');
-   });
-}
+  let botonAdd = document.querySelector(".wrapper-boton");
+  botonAdd.addEventListener("click", () => {
+    let modal = document.querySelector(".modal-wrapper");
+    modal.setAttribute("style", "display:block");
+  });
+};
 
 addNewEmployee();
 
+/* Fin de agregar empleado */
+/* Desaparecer modal */
 const cancelModal = () => {
-
-    console.log('98172938172');
-    let botonCancel = document.querySelectorAll('.boton-cancel');
-    botonCancel.forEach(boton => {
-        boton.addEventListener('click', () => {
-            let modal = document.querySelector('.modal-wrapper');
-            console.log(boton);
-            modal.setAttribute('style', 'display:none');
-        });
-    })
-    
-}
+  let botonCancel = document.querySelectorAll(".boton-cancel");
+  botonCancel.forEach(boton => {
+    boton.addEventListener("click", () => {
+      let modal = document.querySelector(".modal-wrapper");
+      console.log(boton);
+      modal.setAttribute("style", "display:none");
+    });
+  });
+};
 cancelModal();
+/* Fin de desaparecer modal */
 
+/* Confirmacion de eliminacion modal */
+
+/* Fin de eliminacion modal */
+
+/* Editar modal  */
+
+/* Fin de editar modal */
+
+/* Filtro y busqueda */
+
+const filtrar = () => {
+  let q = document.querySelector("#input-filtro").value;
+  getBuscar(q);
+};
+
+let botonBuscar = document.querySelector("#boton-filtro");
+botonBuscar.addEventListener("click", filtrar);
+/* Fin de filtro y busqueda */
--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 /* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
-
+const baseUrl = "https://tp-js-2-api-wjfqxquokl.now.sh/users";
+let lista = [];
+let todo = {
+  fullname: "",
+  email: "",
+  address: "",
+  phone: ""
+};
+const handleError = err => {
+  alert(`Hubo un error. ${err}`);
+};
 
 /*Funciones A */
-
+const createTodo = async (fullname, email, address, phone) => {
+  try {
+    let data = {
+      fullname,
+      email,
+      address,
+      phone
+    };
+    const res = await axios.post(baseUrl, data);
+    lista.push(res.data);
+  } catch (err) {
+    handleError(err);
+  }
+};
 
 /*Funciones B */
+/* accion filtrar */
+const getLista = async () => {
+  await axios
+    .get(`${baseUrl}`)
+    .then(res => {
+      lista = res.data;
+      showTodo();
+    })
+    .catch(handleError);
+};
+
+const getBuscar = async q => {
+  await axios
+    .get(`${baseUrl}/?search=${q}`)
+    .then(res => {
+      lista = res.data;
+    })
+    .catch(handleError);
+};
+
+/* validar campos llenos */
 
 /*Funciones C eliminar*/
+const deleteTodo = async (id, callback) => {
+  try {
+    const res = await axios.delete(`${baseUrl}/:${id}`);
+    const index = lista.findIndex(todo => {
+      return todo.id == id;
+    });
+    lista.splice(index, 1);
+    callback();
+  } catch (err) {
+    handleError(err);
+  }
+};
 
-/*Funciones D */
+getLista();
