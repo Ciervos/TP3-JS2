4cf281a33fc91177a462e920bf97918939150fcc
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-02-03 00:14:02 -0300

    se agrega funcion eliminar

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 /* Estos comentarios con nombres seran borrados, es para que cada una trabaje en su parte sin pisarse, luego en el merge final movemos bien si no quedaron en orden */
-
-
 const baseUrl = "https://tp-js-2-api-wjfqxquokl.now.sh/users";
 let lista = [];
 let todo = {
@@ -15,7 +13,22 @@ const handleError = err =>{
 };
 
 /*Funciones A */
-
+const createTodo = async (fullname, email, address, phone) => {
+   
+    try {
+        let data = {
+            fullname,
+            email,
+            address,
+            phone,
+        }
+        const res = await axios.post(baseUrl, data);
+        lista.push(res.data);
+        
+    } catch (err) {
+    handleError(err);
+    }
+ }
 
 /*Funciones B */
 /* accion filtrar */
@@ -27,7 +40,7 @@ const getLista = async () => {
         })
         .catch(handleError);
 };
-getLista();
+
 
 const getBuscar = async q => {
     await axios.get(`${baseUrl}/?search=${q}`)
@@ -38,74 +51,22 @@ const getBuscar = async q => {
 };
 
 
-
-const filtrar = () =>{
-    let q = document.querySelector("#input-filtro").value; 
-    getBuscar(q);
-}
-
-let botonBuscar = document.querySelector("#boton-filtro");
-botonBuscar.addEventListener("click", filtrar);
-
 /* validar campos llenos */
 
 /*Funciones C eliminar*/
-
-const showTodo = () => {
-    const tbody = document.querySelector('.tabla-contenido');
-    for(let todo of lista) {
-
-        console.log(todo);
-
-        const tr = document.createElement('tr');
-        tr.className= 'tabla-contenido-fila';
-
-        const td = document.createElement('td');
-        td.className ='tabla-contenido-celda';
-        const input =document.createElement('input');
-        input.type = 'checkbox';
-        td.appendChild(input);
-
-        const td1 = document.createElement('td');
-        const spanName = document.createElement('span');
-        spanName.textContent = todo.fullname;
-        td1.appendChild(spanName);
-
-        const td2 = document.createElement('td');
-        const spanEmail = document.createElement('span');
-        spanEmail.textContent = todo.email;
-        td2.appendChild(spanEmail);
-
-        const td3 = document.createElement('td');
-        const spanAddress = document.createElement('span');
-        spanAddress.textContent = todo.address;
-        td3.appendChild(spanAddress);
-
-        const td4 = document.createElement('td');
-        const spanPhone = document.createElement('span');
-        spanPhone.textContent = todo.phone;
-        td4.appendChild(spanPhone);
-
-        const td5 = document.createElement('td');
-        const span = document.createElement('span');
-        span.className = 'content-icons'
-        const i = document.createElement('i');
-        i.className = 'fas fa-pencil-alt pencil';
-        span.appendChild(i);
-        const i1= document.createElement('i');
-        i1.className = 'fas fa-trash-alt tacho';
-        span.appendChild(i1);
-       
-        td5.appendChild(span);
-        tr.appendChild(td);
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
-        tr.appendChild(td5);
-        tbody.appendChild(tr);
+const deleteTodo = async (id, callback) => {
+    try {
+        const res = await axios.delete(`${baseUrl}/:${id}`);
+        const index = lista.findIndex(todo => {
+            return todo.id == id;
+        })
+        lista.splice(index, 1);
+        callback();
+        
+    } catch (err) {
+        handleError(err)
     }
-}
+ }
 
 
-showTodo();
+getLista();
